f31f1c186d5deab0ed167a2bedfd593c962cc901
Pasha <pashaie@gmail.com>
2023-08-22 19:27:47 +0330

    goto

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,10 +1,14 @@
-import { View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
 import React from "react";
 
-export default function HomeScreen() {
+export default function HomeScreen({ navigation }: any) {
+  const gotoDrugList = () => {
+    navigation.push("DrugList");
+  };
   return (
     <View>
       <Text>HomeScreen</Text>
+      <Button title="Drug List" onPress={gotoDrugList}></Button>
     </View>
   );
 }
