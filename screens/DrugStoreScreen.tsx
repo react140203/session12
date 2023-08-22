9065707aa01817583cb4b44413c97eb34c7201bb
Pasha <pashaie@gmail.com>
2023-08-22 21:26:39 +0330

    map

--- screens/DrugStoreScreen.tsx
+++ screens/DrugStoreScreen.tsx
@@ -1,10 +1,46 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, StyleSheet } from "react-native";
+import React, { useEffect, useState } from "react";
+import { supabase } from "../api";
+import MapView, { Marker } from "react-native-maps";
 
 export default function DrugStoreScreen() {
+  const [DrugStores, setDrugStores] = useState<any[]>([]);
+  useEffect(() => {
+    (async () => {
+      const { data, error } = await supabase
+        .from("DrugStore")
+        .select("*")
+        .limit(10);
+      setDrugStores(data);
+      // console.log(data);
+    })();
+  }, []);
+
   return (
-    <View>
-      <Text>DrugStore</Text>
+    <View style={styles.container}>
+      <MapView style={styles.map}>
+        {DrugStores &&
+          DrugStores.map((x) => (
+            <Marker
+              key={x.id}
+              coordinate={{
+                latitude: x.latitude,
+                longitude: x.longitude,
+              }}
+              title={x.name}
+            ></Marker>
+          ))}
+      </MapView>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: "100%",
+    height: "100%",
+  },
+});
