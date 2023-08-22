4acd7eeb3b7e6d2b46addaadfb0c880f284cf083
Pasha <pashaie@gmail.com>
2023-08-22 19:22:47 +0330

    stack navigation

--- App.tsx
+++ App.tsx
@@ -1,14 +1,19 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "./screens/HomeScreen";
+import DrugListScreen from "./screens/DrugListScreen";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <View style={styles.container}>
-        <Text>Open up App.tsx to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </View>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="DrugList" component={DrugListScreen} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
